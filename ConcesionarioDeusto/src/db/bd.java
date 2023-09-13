6adf098d617e3775dfdc3d1790d93886ba1f947d
ignacioechavarren <ignacioechavarren@opendeusto.es>
2023-09-13 12:59:02 +0200

    Organización carpetas

---
+++ ConcesionarioDeusto/src/db/bd.java
@@ -0,0 +1,10 @@
+package db;
+
+public class bd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ConcesionarioDeusto/src/domain/prueba.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
