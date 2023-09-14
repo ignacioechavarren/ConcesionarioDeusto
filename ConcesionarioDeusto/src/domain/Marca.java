9caf18087277cad242e773d60b17e9cbaabbbf11
daniel.nocito <daniel.nocito@E109L1.opendeusto.es>
2023-09-14 16:36:16 +0200

    marca modf

--- ConcesionarioDeusto/src/domain/Marca.java
+++ ConcesionarioDeusto/src/domain/Marca.java
@@ -1,5 +1,52 @@
 package domain;
-
-public enum Marca {
-
+import java.io.Serializable;
+public enum Marca implements Serializable{
+	FER("Ferrari"),
+	BMW("BMW"), 
+	AU("Audi"), 
+	REN("Renault"),
+	MER("Mercedes-Benz"), 
+	BUG("Bugatti"),
+	BEN("Bentley"),
+	ALF("Alfa Romeo"),
+	MUS("Mustang"),
+	CIT("Citroen"),
+	DAC("Dacia"),
+	FIA("Fiat"),
+	FOR("Ford"),
+	SEA("Seat"),
+	HON("Honda"),
+	HYU("Hyundai"),
+	JAG("Jaguar"),
+	JEE("Jeep"),
+	KIA("Kia"),
+	LAM("Lamborghini"),
+	LAN("Land Rover"),
+	LEX("Lexus"),
+	MAS("Maserati"),
+	MAZ("Mazda"),
+	MIN("Mini"),
+	MIT("Mitsubishi"),
+	NIS("Nissan"),
+	OPE("Opel"),
+	PEU("Peugeot"),
+	POR("Porsche"),
+	SKO("Skoda"),
+	SMA("Smart"),
+	SUB("Subaru"),
+	SUZ("Suzuki"),
+	TES("Tesla"),
+	TOY("Toyota"),
+	VOL("Volkswagen");
+	private String nombre;
+	
+	Marca(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getMarca() {
+		return this.nombre;
+	}
+	
+	
 }
