34b428125829f84ed4ffaf85464cdb863cefc2bb
ignacioechavarren <ignacioechavarren@opendeusto.es>
2023-10-10 15:25:19 +0200

    Cambios minimos

--- ConcesionarioDeusto/src/domain/Cliente.java
+++ ConcesionarioDeusto/src/domain/Cliente.java
@@ -2,12 +2,24 @@ package domain;
 
 public class Cliente {
     
+	/**
+	 * 
+	 */
     private int id;
     private String nombre;
     private String apellido;
     private String direccion;
     private String telefono;
 
+    
+   /**
+    * 
+    * @param id
+    * @param nombre
+    * @param apellido
+    * @param direccion
+    * @param telefono
+    */
     public Cliente(int id, String nombre, String apellido, String direccion, String telefono) {
         this.id = id;
         this.nombre = nombre;
@@ -15,7 +27,9 @@ public class Cliente {
         this.direccion = direccion;
         this.telefono = telefono;
     }
-    
+    /**
+     * 
+     */
     public Cliente() {
         this.id = 0;
         this.nombre = "";
@@ -24,52 +38,97 @@ public class Cliente {
         this.telefono = "";
     }
 
+    /**
+     * 
+     * @return
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * 
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
-
+    
+    /**
+     * 
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     * 
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    /**
+     * 
+     * @return
+     */
     public String getApellido() {
         return apellido;
     }
+    
+    /**
+     * 
+     * @param apellido
+     */
 
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
-
+    
+    /**
+     * 
+     * @return
+     */
     public String getDireccion() {
         return direccion;
     }
+    
+    /**
+     * 
+     * @param direccion
+     */
 
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+    /**
+     * 
+     * @return
+     */
     public String getTelefono() {
         return telefono;
     }
-
+    
+    /**
+     * 
+     * @param telefono
+     */
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
+    /**
+     * 
+     */
     @Override
 	public String toString() {
 		return "Cliente [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion
 				+ ", telefono=" + telefono + "]";
 	}
-
+    /**
+     * 
+     */
 	public void mostrarInformacion() {
         System.out.println("ID: " + id);
         System.out.println("Nombre: " + nombre);
@@ -77,7 +136,10 @@ public class Cliente {
         System.out.println("Direccion: " + direccion);
         System.out.println("Telefono: " + telefono);
     }
-
+	/**
+	 * 
+	 * @param args
+ */
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente(1, "Juan", "Perez", "Parque de los Lirios", "678458377");
         cliente1.mostrarInformacion();
--- ConcesionarioDeusto/src/domain/Coche.java
+++ ConcesionarioDeusto/src/domain/Coche.java
@@ -1,12 +1,23 @@
 package domain;
 
 public class Coche {
-
+	/**
+	 * 
+	 */
 	private double precio;
 	private int anyo;
 	private String modelo;
 	private String marca;
 	private Matricula matricula;
+	
+	/**
+	 * 
+	 * @param precio
+	 * @param anyo
+	 * @param modelo
+	 * @param marca
+	 * @param matricula
+	 */
 	
 	public Coche(double precio, int anyo, String modelo, String marca, Matricula matricula) {
 		super();
@@ -17,6 +28,10 @@ public class Coche {
 		this.matricula = matricula;
 	}
 	
+	/**
+	 * 
+	 */
+	
 	public Coche() {
 		super();
 		this.precio = 0.0;
@@ -25,53 +40,90 @@ public class Coche {
 		this.marca = "";
 		this.matricula = new Matricula();
 	}
-
+	/**
+	 * 
+	 * @return
+ */
 	public double getPrecio() {
 		return precio;
 	}
-
+	/**
+	 * 
+	 * @param precio
+	 */
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public int getAnyo() {
 		return anyo;
 	}
-
+	/**
+	 * 
+	 * @param anyo
+	 */
 	public void setAnyo(int anyo) {
 		this.anyo = anyo;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String getModelo() {
 		return modelo;
-	}
+	}	
+	/**
+	 * @param modelo
+	 */
 
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
+	/**
+	 * 
+	 * @return
+	 */
 
 	public String getMarca() {
 		return marca;
 	}
+	/**
+	 * 
+	 * @param marca
+	 */
 
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public Matricula getMatricula() {
 		return matricula;
 	}
+	/**
+	 * 
+	 * @param matricula
+	 */
 
 	public void setMatricula(Matricula matricula) {
 		this.matricula = matricula;
 	}
-
+	/**
+	 * 
+	 */
 	@Override
 	public String toString() {
 		return "Coche [precio=" + precio + ", anyo=" + anyo + ", modelo=" + modelo + ", marca=" + marca + ", matricula="
 				+ matricula + "]";
 	}
-	
+	/**
+	 * 
+	 */
 	public void mostrarInformacion() {
         System.out.println("Precio: " + precio);
         System.out.println("Año: " + anyo);
@@ -79,7 +131,10 @@ public class Coche {
         System.out.println("Marca: " + marca);
         System.out.println("Matricula: " + matricula);
     }
-	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
         Coche coche1 = new Coche(1, 2000, "R8", "AUD", new Matricula());
         coche1.mostrarInformacion();
