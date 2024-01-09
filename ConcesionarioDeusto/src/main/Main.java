9099441e4b15f180bf112bef0bc37625f71c9557
gpb117 <gonzalo.pena@opendeusto.es>
2024-01-09 10:28:27 +0100

    limpieza

--- ConcesionarioDeusto/src/main/Main.java
+++ ConcesionarioDeusto/src/main/Main.java
@@ -34,36 +34,9 @@ public class Main {
 			e.printStackTrace();
 		}
 
-		Coche c1 = new Coche(500, 1996, "Ibiza",Marca.SEA,"8828CUP" );
-		Coche c2 = new Coche(800, 2000, "E46",Marca.BMW,"4777CHD" );
-				
-		Cliente cli1 = new Cliente("16102892V", "Jon", "14-10-1999", "A");
-		Cliente cli2 = new Cliente("16122842V", "Alex", "04-10-2009", "B");
-				
-		Concesionario.imprimirCoches();
-		
-		Concesionario.cargarClientesEnLista("Clientes.csv");
-		System.out.println("Lista de clientes antes de ordenarla");
-		Concesionario.imprimirClientes();
-		Concesionario.ordenarListaClientes();
-		System.out.println("Lista de clientes después de ordenarla");
-		Concesionario.imprimirClientes();
-		
-		Concesionario.aniadirReserva(cli1, c1);
-		Concesionario.aniadirReserva(cli2, c2);
-		Concesionario.aniadirReserva(cli1, c2);
-		System.out.println("TODAS LAS RESERVAS");
-		Concesionario.imprimirReservas();
-		System.out.println("Reservas del cliente: "+cli1.getDni());
-		Concesionario.imprimirReservasCliente(cli1);
-		System.out.println("************************************************************************");
-		
-		VentanaInicio vi=new VentanaInicio(conc);		
-		
+		VentanaInicio vi=new VentanaInicio(conc);			
 		
 	}
-
-
-	}
+}
 
 
